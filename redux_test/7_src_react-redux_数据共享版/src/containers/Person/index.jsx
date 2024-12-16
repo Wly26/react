4e78a638db4cd66a6c554afeb10129d65d98d2ac
Wly26 <1402404992@qq.com>
2024-12-16 17:00:39 +0800

    7_src_react-redux_数据共享版

---
+++ redux_test/7_src_react-redux_数据共享版/src/containers/Person/index.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import {nanoid} from 'nanoid'
+import { connect } from 'react-redux'
+import {createAddPersonAction} from '../../redux/actions/person'
+
+class Person extends Component {
+    nameNode = React.createRef()
+    ageNode = React.createRef()
+    addPerson = ()=>{
+        const name = this.nameNode.current.value;
+        const age = this.ageNode.current.value;
+        const personObj = {id:nanoid(),name,age}
+        this.props.add(personObj)
+        this.nameNode.current.value = '';
+        this.ageNode.current.value = ''
+    }
+    render() {
+        return (
+        <div>
+            <p>上方组件，总数：{this.props.sum}</p>
+            <input ref={this.nameNode} type='text' placeholder='输入名字'/>
+            <input ref={this.ageNode} type='text' placeholder='输入年龄'/>
+            <button onClick={this.addPerson}>添加</button>
+            <ul>
+                {/* <li>名字1————年龄1</li>
+                <li>名字2————年龄2</li>
+                <li>名字3————年龄3</li> */}
+                {
+                    this.props.people.map((data)=>{
+                        return <li key={data.id}>{data.name}————{data.age}</li>
+                    })
+                }
+            </ul>
+        </div>
+        )
+    }
+}
+export default connect(
+    state => ({people:state.rens,sum:state.he}),
+    {
+        add:createAddPersonAction
+    }
+
+)(Person)
---
+++ redux_test/7_src_react-redux_数据共享版/src/index.js
@@ -0,0 +1,15 @@
+//引入react核心库
+import React from 'react'
+//引入ReactDOM
+import ReactDOM from 'react-dom/client'
+//引入App
+import App from './App'
+import store from './redux/store'
+import {Provider} from 'react-redux'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+);
