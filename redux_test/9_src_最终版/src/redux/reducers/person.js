20d2cca140c583b0a30ab0f6c672006863422052
Wly26 <1402404992@qq.com>
2024-12-16 20:34:54 +0800

    9_src_最终版

--- redux_test/9_src_最终版/src/redux/reducers/person.js
+++ redux_test/9_src_最终版/src/redux/reducers/person.js
@@ -2,6 +2,7 @@ import {ADD_PERSON} from '../constant'
 
 //初始化人的列表
 const initState = [{id:'001',name:'tom',age:18}]
+
 // 这是一个纯函数，不能改写状态
 export default function personReducer(preState=initState,action){
 	// console.log('personReducer@#@#@#');
