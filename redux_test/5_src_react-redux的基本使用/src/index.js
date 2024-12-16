5baffbd108718c4f47f721b844a6ceb024ba3c23
Wly26 <1402404992@qq.com>
2024-12-16 13:27:54 +0800

    5_src_react-redux的基本使用

---
+++ redux_test/5_src_react-redux的基本使用/src/index.js
@@ -0,0 +1,18 @@
+//引入react核心库
+import React from 'react'
+//引入ReactDOM
+import ReactDOM from 'react-dom/client'
+//引入App
+import App from './App'
+import store from './redux/store'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
+// 更新状态
+store.subscribe(()=>{
+    root.render(
+        <App />
+    );
+})
