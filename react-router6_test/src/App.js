ba62bf09b6acdd467d592537b68a43aad9853e7a
Wly26 <1402404992@qq.com>
2024-12-20 19:34:37 +0800

    router6

---
+++ react-router6_test/src/App.js
@@ -0,0 +1,13 @@
+// 创建“外壳”组件app
+import React, { Component } from 'react'
+
+// 创建并暴露app组件
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+				app
+			</div>
+		)
+	}
+}
