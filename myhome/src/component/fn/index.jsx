d1601aed736054d65d6b8d7d9bc7585fac3c95ef
Wly26 <1402404992@qq.com>
2024-12-21 12:01:13 +0800

    函数式组件

--- myhome/src/component/fn/index.jsx
+++ myhome/src/component/fn/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client';
 
 //类式组件
-/* class Demo extends React.Component {
+/* class Fn extends React.Component {
 
 	state = {count:0}
 
@@ -44,12 +44,22 @@ import ReactDOM from 'react-dom'
 } */
 
 // 函数式组件
-function Demo(){
-	//console.log('Demo');
+function Fn(){
+	// ref
+	const myRef = React.useRef();
 
+	// React.useState() 返回一个数组【状态，自定义一个更新状态的方法名】
+	// React.useState(0) 是指：传入了一个状态
+	// React.useState会把原始值存起来，更新状态的时候，不会被覆盖
 	const [count,setCount] = React.useState(0)
-	const myRef = React.useRef()
-
+	//加的回调
+	function add(){
+		//setCount(count+1) //第一种写法
+		setCount(count => count+1 )
+	}
+	// 使用生命周期钩子
+	// useEffect（()=>{自定义函数}，[检测到某个数据的改变，而跟着调用]）
+	// []意味着谁都不检测
 	React.useEffect(()=>{
 		let timer = setInterval(()=>{
 			setCount(count => count+1 )
@@ -58,32 +68,28 @@ function Demo(){
 			clearInterval(timer)
 		}
 	},[])
-
-	//加的回调
-	function add(){
-		//setCount(count+1) //第一种写法
-		setCount(count => count+1 )
+	//卸载组件的回调
+	function unmount(){
+		// 会有个报错，但是这里只是一个案例，不用在意
+        const root = ReactDOM.createRoot(document.getElementById('root'));
+        root.unmount();
 	}
-
 	//提示输入的回调
 	function show(){
 		alert(myRef.current.value)
 	}
 
-	//卸载组件的回调
-	function unmount(){
-		ReactDOM.unmountComponentAtNode(document.getElementById('root'))
-	}
 
 	return (
 		<div>
+			<p>这是一个函数式组件</p>
 			<input type="text" ref={myRef}/>
 			<h2>当前求和为：{count}</h2>
 			<button onClick={add}>点我+1</button>
 			<button onClick={unmount}>卸载组件</button>
-			<button onClick={show}>点我提示数据</button>
+			<button onClick={show}>点我提示数据</button> 
 		</div>
 	)
 }
 
-export default Demo
+export default Fn
