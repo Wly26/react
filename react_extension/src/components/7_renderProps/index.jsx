a9d58f42c0b3ab30243ba05f9025ad6c533aa771
Wly26 <1402404992@qq.com>
2025-02-16 21:15:41 +0800

    补充笔记

--- react_extension/src/components/7_renderProps/index.jsx
+++ react_extension/src/components/7_renderProps/index.jsx
@@ -5,12 +5,13 @@ import C from '../1_setState'
 export default class Parent extends Component {
 	render() {
 		return (
-			<div className="parent">
-				<h3>我是Parent组件</h3>
-				{/* 把C组件当一个函数，给A进行渲染。 */}
-				<A render={(name)=><C name={name}/>}/>
-			</div>
-		)
+      <div className="parent">
+        <h3>我是Parent组件</h3>
+        {/* 把C组件当一个函数，给A进行渲染。 */}
+        <A render={(name) => <C name={name} />} />
+        <B render={(name) => <C name={name} />} />
+      </div>
+    );
 	}
 }
 
@@ -31,10 +32,10 @@ class A extends Component {
 
 class B extends Component {
 	render() {
-		console.log('B--render');
+		console.log("B--render", this.props);
 		return (
 			<div className="b">
-				<h3>我是B组件,{this.props.name}</h3>
+				<h3>我是B组件</h3>
 			</div>
 		)
 	}
